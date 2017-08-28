adfa4beb442e1754f5d8afc5a0835b9fbe427f77
Saleem Rashid <dev@saleemrashid.com>
2017-08-28 13:07:44 +0100

    trezor: Center OLED on display

--- trezor/oled.c
+++ trezor/oled.c
@@ -4,6 +4,15 @@
 
 uint8_t display[DISPLAY_SIZE];
 
+#define DISPLAY_OFFSET_X ((DISPLAY_WIDTH  - OLED_WIDTH)  / 2)
+#define DISPLAY_OFFSET_Y ((DISPLAY_HEIGHT - OLED_HEIGHT) / 2)
+
+static inline void display_draw(uint8_t x, uint8_t y, bool set) {
+    uint8_t color = set ? GColorWhiteARGB8 : GColorBlackARGB8;
+
+    display_set(display, DISPLAY_OFFSET_X + x, DISPLAY_OFFSET_Y + y, color);
+}
+
 void __wrap_oledInit(void) {
     display_setup();
 
@@ -19,9 +28,7 @@ void __wrap_oledRefresh(void) {
 	uint8_t y = (OLED_BUFSIZE - 1 - i) / OLED_WIDTH * 8 + 7;
 
 	for (uint8_t shift = 0; shift < 8; shift++, y--) {
-	    uint8_t color = (oledbuffer[i] >> shift) & 1 ? GColorWhiteARGB8 : GColorBlackARGB8;
-
-	    display_set(display, x, y, color);
+	    display_draw(x, y, (oledbuffer[i] >> shift) & 1);
 	}
     }
 
