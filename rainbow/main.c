bd08a2294f247d992b10299b3112654c2f45629f
Saleem Rashid <dev@saleemrashid.com>
2017-08-28 18:30:58 +0100

    rainbow: Draw larger blocks

--- rainbow/main.c
+++ rainbow/main.c
@@ -6,6 +6,16 @@
 
 uint8_t display[DISPLAY_SIZE];
 
+#define BLOCK_SIZE 24
+
+static inline void draw_block(uint8_t *buffer, uint8_t _x, uint8_t _y, uint8_t _w, uint8_t _h, uint8_t color) {
+    for (uint8_t x = _x, w = _w; w > 0 && x < DISPLAY_WIDTH; x++, w--) {
+	for (uint8_t y = _y, h = _h; h > 0 && y < DISPLAY_HEIGHT; y++, h--) {
+	    display_set(buffer, x, y, color);
+	}
+    }
+}
+
 int main(void) {
     usart_setup();
     puts("\nHello World");
@@ -24,18 +34,12 @@ int main(void) {
 
     uint8_t index = 0;
     while (true) {
-        for (uint8_t x = 0; x < DISPLAY_WIDTH; x += 2) {
+        for (uint8_t x = 0; x < DISPLAY_WIDTH; x += BLOCK_SIZE) {
 	    index++;
 
-            for (uint8_t y = 0; y < DISPLAY_HEIGHT; y += 2) {
+            for (uint8_t y = 0; y < DISPLAY_HEIGHT; y += BLOCK_SIZE) {
 		index = (index + 1) % sizeof(colors);
-
-		uint8_t color = colors[index];
-
-                display_set(display, x,     y, color);
-                display_set(display, x,     y + 1, color);
-                display_set(display, x + 1, y, color);
-                display_set(display, x + 1, y + 1, color);
+		draw_block(display, x, y, BLOCK_SIZE, BLOCK_SIZE, colors[index]);
             }
 
 	    display_frame_begin(display);
